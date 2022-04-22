250b323d9ad9e56a12ec9ad49ade6b54d8998780
felipemaxplay <felipemaxplay@gmail.com>
2022-04-22 17:32:13 -0300

    init currency-exchange-service

---
+++ currency-exchange-service/src/test/java/br/com/felipemaxplay/microservice/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipemaxplay.microservice.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

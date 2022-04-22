659232c0dbb53430672cec7fdac1569bc38df145
felipemaxplay <felipemaxplay@gmail.com>
2022-04-21 21:27:01 -0300

    init spring-cloud-config-server

---
+++ spring-cloud-config-server/src/test/java/br/com/felipemaxplay/microservice/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipemaxplay.microservice.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

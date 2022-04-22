da13ae3dc24df4f1674879366027d6d07dd90828
felipemaxplay <felipemaxplay@gmail.com>
2022-04-21 21:24:22 -0300

    Enhance limits service get configuration from app props

--- limits-service/src/main/java/br/com/felipemaxplay/microservice/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/br/com/felipemaxplay/microservice/limitsservice/controller/LimitsController.java
@@ -1,6 +1,7 @@
 package br.com.felipemaxplay.microservice.limitsservice.controller;
 
 import br.com.felipemaxplay.microservice.limitsservice.bean.Limits;
+import br.com.felipemaxplay.microservice.limitsservice.config.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/limits")
 public class LimitsController {
 
+    private Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+//        return new Limits(1, 1000);
     }
 }

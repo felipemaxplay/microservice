1304e286a20723dc8f52c07b0507a3d4edacbe63
felipemaxplay <felipemaxplay@gmail.com>
2022-04-23 16:13:16 -0300

    change data.sql values

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into currency_exchanges
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10001,'USD','INR',65,'');
+values(10001,'USD','BRL',4.8,'');
 insert into currency_exchanges
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10002,'EUR','INR',75,'');
+values(10002,'EUR','BRL',5.19,'');
 insert into currency_exchanges
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10003,'AUD','INR',25,'');
+values(10003,'AUD','BLR',3.48,'');

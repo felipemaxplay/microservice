6ad6bd6085fb7204d58d7ed4d7a0fd6ab4c3da46
felipemaxplay <felipemaxplay@gmail.com>
2022-04-23 16:14:45 -0300

    correct the name BLR to BRL

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -6,4 +6,4 @@ insert into currency_exchanges
 values(10002,'EUR','BRL',5.19,'');
 insert into currency_exchanges
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10003,'AUD','BLR',3.48,'');
+values(10003,'AUD','BRL',3.48,'');

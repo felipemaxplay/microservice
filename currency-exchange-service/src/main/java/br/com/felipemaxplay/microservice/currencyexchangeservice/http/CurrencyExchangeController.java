5e738c1e57b31802100fa1e86522ff7a57a79057
felipemaxplay <felipemaxplay@gmail.com>
2022-05-02 10:40:41 -0300

    connecting currency-exchange-service with Zipkin

--- currency-exchange-service/src/main/java/br/com/felipemaxplay/microservice/currencyexchangeservice/http/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/br/com/felipemaxplay/microservice/currencyexchangeservice/http/CurrencyExchangeController.java
@@ -2,17 +2,19 @@ package br.com.felipemaxplay.microservice.currencyexchangeservice.http;
 
 import br.com.felipemaxplay.microservice.currencyexchangeservice.model.CurrencyExchange;
 import br.com.felipemaxplay.microservice.currencyexchangeservice.repository.CurrencyExchangeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/currency-exchange")
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     private final CurrencyExchangeRepository currencyExchangeRepository;
     private final Environment environment;
 
@@ -23,6 +25,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to) {
+        logger.info("Retrieve exchange value called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if(currencyExchange == null) {
             throw new RuntimeException(String.format("Unable to find data for from %s, to %s", from, to));
